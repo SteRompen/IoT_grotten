1b9b8b45d3a7f40ed5e3b3f257c71cd0047844e7
Stefan Rompen <90628031+SteRompen@users.noreply.github.com>
2023-06-12 14:01:01 +0200

    Update main.py

--- project/main.py
+++ project/main.py
@@ -2,6 +2,7 @@
 from bme680 import *
 from machine import I2C, Pin
 from time import sleep
+import random
 
 
 # Initalisatie en declaratie -------------------------------------------------------------------
@@ -14,27 +15,33 @@ bme = BME680_I2C(I2C1)
 time.sleep(1)
 # Data dict
 measurement = {
-  "temperature": None,
-  "humidity": None,
-  "pressure": None,
-  "air_quality": None,
-  "gas": None
+    "device_id": 1,
+    "measurement_id": None,
+    "temperature": None,
+    "humidity": None,
+    "pressure": None,
+    "air_quality": None,
+    "gas": None,
+    "carbon_dioxide": None
 }
 # Onboard LED initaliseren (enkel voor test, in het echt wordt deze uitgeschakeld om stroom te besparen!
 led = Pin("LED", Pin.OUT)
 
+
 # Bijstellen -----------------------------------------------------------------------------------
 # Normale luchtdruk locatie (hPa) op NAP
-sealevelpressure = 1015.2
+sea_level_pressure = 1015.2
 # Deze variable wordt gebruikt ter correctie vd offset van de temperatuur. 
 temperature_offset = -1
+
 
 # Code -----------------------------------------------------------------------------------------
 def measureClimate():
     led.value(1)
     # Reset van de sea_level variabele op basis van eigen variabele
     if 'sealevelpressure' in locals():
-        bme.sea_level_pressure = sealevelpressure
+        bme.sea_level_pressure = sea_level_pressure
+    measurement["measurement_id"] = random.randint(0, 100)
     # Sla de waarde die gemeten is op in de dictonairy
     measurement["air_quality"] = calculateIAQ(bme.gas)
     measurement["temperature"] = bme.temperature
@@ -42,6 +49,7 @@ def measureClimate():
     measurement["pressure"] = bme.pressure
     measurement["gas"] = bme.gas
     led.value(0)
+
 
 def calculateIAQ(gas):
     # Calculate the air_quality_score using temperature, humidity, and gas_resistance
@@ -56,4 +64,5 @@ while True:
     measureClimate()  
     print(measurement)
     time.sleep(3)
+
 
